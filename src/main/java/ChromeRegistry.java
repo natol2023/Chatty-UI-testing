78e978197af068b67e19bafc0f5c1b1580187145
natol2023 <moneta.ya@gmail.com>
2024-07-01 20:04:33 +0200

    Maven & ChromeRegistry class

---
+++ src/main/java/ChromeRegistry.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeRegistry {
+    private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+    private static final String PATH = "." +  FILE_SEPARATOR +  "src" +  FILE_SEPARATOR + "main" +  FILE_SEPARATOR
+            + "resources" +  FILE_SEPARATOR + FILE_SEPARATOR +  "chromedriver.exe";
+
+    public ChromeDriver registerDriver () {
+        System.setProperty("webdriver.chrome.driver", PATH);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        return new ChromeDriver();
+    }
+}
