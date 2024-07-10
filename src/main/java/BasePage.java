8789a8044c671545b535aaf691f23f514288ee43
natol2023 <moneta.ya@gmail.com>
2024-07-10 19:27:42 +0200

    BasePage added

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.support.PageFactory;
 public class BasePage {
     protected WebDriver driver;
 
-
     public BasePage(WebDriver driver) {
         this.driver = driver;
 
