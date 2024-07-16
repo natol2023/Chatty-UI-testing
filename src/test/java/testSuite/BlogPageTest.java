a659e324fcf6f0c3259486ff266639eaf312aed2
Alona <batrakalena86@gmail.com>
2024-07-16 16:30:38 +0200

    add_BlogPageTest_

--- src/test/java/testSuite/BlogPageTest.java
+++ src/test/java/testSuite/BlogPageTest.java
@@ -25,50 +25,42 @@ public class BlogPageTest extends BaseTest {
     }
     @Test
     public void successCreatePost() throws InterruptedException {
-    ChromeDriver driver = new ChromeRegistry().registerDriver();
-    driver.get("http://chatty.telran-edu.de:8089/login");
-    LoginPage loginPage = new LoginPage(driver, wait);
-    loginPage.inputEmail();
-    loginPage.inputPassword();
-    loginPage.clickLogin();
-
-    BlogPage blogPage = new BlogPage(driver, wait);
-    String postTitle = "Nature";
-    sleep(5000);
-    blogPage.clickCreatePost();
-    blogPage.titleInput();
-    blogPage.descriptionInput();
-    blogPage.contentInput();
-    sleep(5000);
-    blogPage.submitButton();
-    sleep(5000);
-    assertTrue(blogPage.isPostPresent(postTitle));
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open()
+                .inputEmail("batrakalena86@gmail.com")
+                .inputPassword("qwerty12345")
+                .clickLoginButton();
+        BlogPage blogPage = new BlogPage(driver);
+        String postTitle = "Nature";
+        sleep(5000);
+        blogPage.clickCreatePost();
+        blogPage.titleInput();
+        blogPage.descriptionInput();
+        blogPage.contentInput();
+        blogPage.submitButton();
+        assertTrue(blogPage.isPostPresent(postTitle));
     }
     @Test
     public void createPostWithAllEmptyFields() throws InterruptedException {
-        ChromeDriver driver = new ChromeRegistry().registerDriver();
-        driver.get("http://chatty.telran-edu.de:8089/login");
-        LoginPage loginPage = new LoginPage(driver, wait);
-        loginPage.inputEmail();
-        loginPage.inputPassword();
-        loginPage.clickLogin();
-
-        BlogPage blogPage = new BlogPage(driver, wait);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open()
+                .inputEmail("batrakalena86@gmail.com")
+                .inputPassword("qwerty12345")
+                .clickLoginButton();
         sleep(500);
+        BlogPage blogPage = new BlogPage(driver);
         blogPage.clickCreatePost();
         blogPage.submitButton();
         assertTrue(blogPage.isContentErrorVisible(), "Please fill the field");
     }
     @Test
     public void successSavePostAsDraft() throws InterruptedException {
-        ChromeDriver driver = new ChromeRegistry().registerDriver();
-        driver.get("http://chatty.telran-edu.de:8089/login");
-        LoginPage loginPage = new LoginPage(driver, wait);
-        loginPage.inputEmail();
-        loginPage.inputPassword();
-        loginPage.clickLogin();
-        BlogPage blogPage = new BlogPage(driver, wait);
-        sleep(5000);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open()
+                .inputEmail("example4@email.com")
+                .inputPassword("example1234")
+                .clickLoginButton();
+        BlogPage blogPage = new BlogPage(driver);
         blogPage.clickCreatePost();
         blogPage.titleInput();
         blogPage.descriptionInput();
